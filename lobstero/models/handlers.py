8ead46a3d2a96b3440864c482d5d4ce9770a5069
Kaylynn Morgan <mkaylynn7@gmail.com>
2020-02-11 12:25:36 +1100

    j

--- lobstero/models/handlers.py
+++ lobstero/models/handlers.py
@@ -131,7 +131,7 @@ class LobsterHandler():
             try:
                 raise error
             except Exception:
-                await self.format_tb_and_send(additional=str(dir(error)))
+                await self.format_tb_and_send(additional=str((dir(error), error.args)))
 
             raise error  # Raise again, now that it's been logged on discord.
 
