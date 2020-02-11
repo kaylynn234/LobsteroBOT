97cee67016fb12fb0644cea654f12efc346df4df
Kaylynn Morgan <mkaylynn7@gmail.com>
2020-02-11 13:23:58 +1100

    let's try again

--- lobstero/models/handlers.py
+++ lobstero/models/handlers.py
@@ -172,60 +172,56 @@ class GreedyMention(commands.Converter):
             raise commands.BadArgument(message=f"Expected str instance, got {type(argument)}")
 
 
-def blueprint_check():
+def blueprint_check(ctx):
     """A preliminary check that enables blueprint functionality.."""
+    res = db.blueprints_for(str(ctx.guild.id), ctx.command.qualified_name)
+    if res is None:
+        return False
+    successful, failed = [], []
 
-    def predicate(ctx):
-        res = db.blueprints_for(str(ctx.guild.id), ctx.command.qualified_name)
-        if res is None:
-            return False
+    for check in res:
+        if check["criteria_type"] == "has_any_role":
+            if bool(ctx.author.roles) is check["criteria_requires"]:
+                successful.append(check)
+            else:
+                failed.append(check)
+        elif check["criteria_type"] == "has_role":
+            role = check["criteria_value"] in [str(x.id) for x in ctx.author.roles]
+            if role is check["criteria_requires"]:
+                successful.append(check)
+            else:
+                failed.append(check)
+        elif check["criteria_type"] == "has_permissions":
+            perms = ctx.author.permissions_in(ctx.channel)
+            can_run = getattr(perms, check["criteria_value"], False)
+            if can_run is check["criteria_requires"]:
+                successful.append(check)
+            else:
+                failed.append(check)
+        elif check["criteria_type"] == "has_strict_permissions":
+            perms = ctx.author.permissions
+            can_run = getattr(perms, check["criteria_value"], False)
+            if can_run is check["criteria_requires"]:
+                successful.append(check)
+            else:
+                failed.append(check)
+        elif check["criteria_type"] == "is_specific_user":
+            can_run = str(ctx.author.id) == check["criteria_value"]
+            if can_run == check["criteria_requires"]:
+                successful.append(check)
+            else:
+                failed.append(check)
+        elif check["criteria_type"] == "is_guild_owner":
+            can_run = str(ctx.author.id) == str(ctx.guild.owner.id)
+            if str(ctx.author.id) == check["criteria_requires"]:
+                successful.append(check)
+            else:
+                failed.append(check)
 
-        successful, failed = [], []
-        for check in res:
-            if check["criteria_type"] == "has_any_role":
-                if bool(ctx.author.roles) is check["criteria_requires"]:
-                    successful.append(check)
-                else:
-                    failed.append(check)
-            elif check["criteria_type"] == "has_role":
-                role = check["criteria_value"] in [str(x.id) for x in ctx.author.roles]
-                if role is check["criteria_requires"]:
-                    successful.append(check)
-                else:
-                    failed.append(check)
-            elif check["criteria_type"] == "has_permissions":
-                perms = ctx.author.permissions_in(ctx.channel)
-                can_run = getattr(perms, check["criteria_value"], False)
-                if can_run is check["criteria_requires"]:
-                    successful.append(check)
-                else:
-                    failed.append(check)
-            elif check["criteria_type"] == "has_strict_permissions":
-                perms = ctx.author.permissions
-                can_run = getattr(perms, check["criteria_value"], False)
-                if can_run is check["criteria_requires"]:
-                    successful.append(check)
-                else:
-                    failed.append(check)
-            elif check["criteria_type"] == "is_specific_user":
-                can_run = str(ctx.author.id) == check["criteria_value"]
-                if can_run == check["criteria_requires"]:
-                    successful.append(check)
-                else:
-                    failed.append(check)
-            elif check["criteria_type"] == "is_guild_owner":
-                can_run = str(ctx.author.id) == str(ctx.guild.owner.id)
-                if str(ctx.author.id) == check["criteria_requires"]:
-                    successful.append(check)
-                else:
-                    failed.append(check)
-
-        if failed:
-            raise BlueprintFailure(ctx.bot, successful, failed)
-        else:
-            return True
-
-    return commands.check(predicate)
+    if failed:
+        raise BlueprintFailure(ctx.bot, successful, failed)
+    else:
+        return True
 
 
 def blueprints_or(c=None):
@@ -234,7 +230,7 @@ def blueprints_or(c=None):
     async def predicate(ctx):
         blueprinted = blueprint_check().predicate
         blueprints_passed = await blueprinted(ctx)
-        # this can raise an error, which will propgate if it does
+        # this can raise an error, which will propogate if it does
 
         # test the non-blueprint check
         if c is not None:
