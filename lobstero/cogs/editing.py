5cc09a5968bc07526b7fbcc89bd669d8e982eaaa
Kaylynn <mkaylynn7@gmail.com>
2020-04-27 20:31:33 +1000

    yeah okay

--- lobstero/cogs/editing.py
+++ lobstero/cogs/editing.py
@@ -905,7 +905,7 @@ If you don't do any of that, Lobstero will search the previous few messages for 
         done = discord.Embed(title="Judgement comes!", color=16202876)
         await snt.edit(embed=done)
         await self.save_and_send(
-            ctx, frames[0], "wheelofban.gif", save_all=True, format="GIF",
+            ctx, frames[0], "wheelofban.gif", save_all=True,
             append_images=frames[1:], optimize=True, loop=0, duration=30
         )
 
